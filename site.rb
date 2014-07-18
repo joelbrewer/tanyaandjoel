a03541840a3e11b04a32aaf4c47b6b1be4d43a55
Joel Brewer <joelbrewer01@gmail.com>
2014-07-17 19:26:43 -0500

    add routes for individual posts

--- site.rb
+++ site.rb
@@ -7,12 +7,22 @@ class Post < ActiveRecord::Base
 end
 
 get '/' do
+  @latest_post = Post.last
   haml :home
 end
 
 get '/blog' do
   @posts = Post.all
   haml :blog
+end
+
+get '/blog/:title' do
+  @post = Post.find_by title: params[:title].capitalize
+  if @post
+    haml :post
+  else
+    haml :error
+  end
 end
 
 get '/style.css' do
