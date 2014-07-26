1b0a546bd40f3bbd1c3c1406ec28fd81009ed33b
Joel Brewer <joelbrewer01@gmail.com>
2014-07-25 20:34:33 -0500

    move deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,44 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'rvm/capistrano'
+require 'bundler/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+#RVM and bundler settings
+set :bundle_cmd, "/Users/joelbrewer/.rvm/gems/ruby-2.0.0-p353@global/bin/bundle"
+set :bundle_dir, "/Users/joelbrewer/.rvm/gems/ruby-2.0.0-p353/gems"
+set :rvm_ruby_string, :local
+set :rack_env, :production
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#general info
+set :user, 'ubuntu'
+set :domain, 'ec2-54-191-201-125.us-west-2.compute.amazonaws.com'
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :applicationdir, "/home/www/tanyaandjoel"
+set :scm, 'git'
+set :application, "tanyaandjoel"
+set :repository,  "git@github.com:jahbrewski/tanyaandjoel.git"
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :deploy_via, :remote_cache
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :deploy_to, applicationdir
+set :deploy_via, :export
+
+#additional settings -- mostly SSH
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "keys/tolkien_key_pair.cer")]
+ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+
+after "deploy:cold" do
+  admin.nginx_restart
+end
+
+namespace :deploy do
+  desc "Not starting as we're running passenger."
+  task :start do
+  end
+end
